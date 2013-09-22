5e1f45dbc1863d389d8ae4214a2b49478ed718d3
Filip Slavov <2332996+NibbleByte@users.noreply.github.com>
2013-09-22 08:41:03 +0000

    * Added IdleAnimationsSystem, triggering idle animations from time to time. * Exposed the available sequence names in the animator.

--- MobileClient/LocalFiles/Javascript/Render/RenderEvents.js
+++ MobileClient/LocalFiles/Javascript/Render/RenderEvents.js
@@ -8,6 +8,6 @@
 // Supported render events that user can subscribe to.
 var RenderEvents = {
 		Animations: {
-			ANIMATION_FINISHED: "render.animations.animation_finished",	// Arguments: event, {name, entity}		
+			ANIMATION_FINISHED: "render.animations.animation_finished",	// Arguments: event, {name, animator, entity}		
 		}
 }
