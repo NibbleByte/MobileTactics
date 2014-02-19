3f0a359a5bd75cda6f6c4b10cf113f1b5c6337f6
NibbleByte <NibbleByte@655ab1b8-11a9-487b-b931-cf28db4b0e7f>
2014-02-19 22:50:35 +0000

    * Enhanced the subscriber to better handle trigger chains (now all triggers are async).

--- trunk/Core/Engine/Utils/Subscriber.js
+++ trunk/Core/Engine/Utils/Subscriber.js
@@ -21,16 +21,43 @@ Subscriber.makeSubscribable = function (obj) {
 	obj.createSubscriber = Subscriber._createSubscriber;
 	
 	obj.trigger = Subscriber._trigger;
+	obj.__triggersQueued = [];
 }
 
 Subscriber._createSubscriber = function () {
 	return new Subscriber(this);
 }
 
-Subscriber._trigger = function(event, data) {
+// Triggers event with given data.
+// If another trigger is already in progress (trigger chain), 
+// the new trigger will be queued and executed when done with the previous ones.
+// It is recommended to use these asynchronous triggers, to avoid more problems.
+Subscriber._trigger = function (event, data) {
 	
 	console.assert(event);
 	
+	// Store the trigger data so it can processed later.
+	this.__triggersQueued.push({ event: event, data: data })
+
+	// If this trigger is the only stored trigger, start executing directly.
+	// Else, there are currently other triggers being executed, so skip.
+	if (this.__triggersQueued.length == 1) {
+
+		while (this.__triggersQueued.length > 0) {
+			var triggerData = this.__triggersQueued[0];
+
+			$(this).trigger(triggerData.event, triggerData.data);
+			this.__triggersQueued.shift();
+		}
+	}
+}
+
+// Triggers event with given data immediately.
+// This is NOT recommended and should be avoided.
+Subscriber._triggerSync = function (event, data) {
+
+	console.assert(event);
+
 	$(this).trigger(event, data);
 }
 
