5dac0693e1f3623f885f13ae10a30f4dd63ff612
NibbleByte <NibbleByte@655ab1b8-11a9-487b-b931-cf28db4b0e7f>
2014-02-21 13:52:37 +0000

    * Enhanced the gatherTiles algorithm to be generic and reusable.

--- trunk/Core/Gameplay/Actions/ActionMove.js
+++ trunk/Core/Gameplay/Actions/ActionMove.js
@@ -10,8 +10,6 @@ Actions.Classes.ActionMove = new function () {
 	this.getAvailableActions = function (eworld, world, player, placeable, outActions) {
 		var tile = placeable.CTilePlaceable.tile;
 		
-		var availableTiles = [];
-		
 		var movementData = {
 			world: world,
 			placeable: placeable,
@@ -19,7 +17,8 @@ Actions.Classes.ActionMove = new function () {
 			playersData: eworld.extract(PlayersData),
 		}
 		
-		gatherMovementTiles(movementData, tile, placeable.CStatistics.statistics['Movement'], availableTiles);
+
+		var availableTiles = world.gatherTiles(tile, placeable.CStatistics.statistics['Movement'], movementCostQuery, movementData);
 		
 		// If nowhere to move, action is unavailable.
 		if (availableTiles.length <= 1) {
@@ -39,75 +38,21 @@ Actions.Classes.ActionMove = new function () {
 	//
 	// Private
 	//
-	var gatherMovementTiles = function (movementData, startTile, movement, availableTiles) {
-		var open = [ startTile ];
-		var visited = [];
-		
-		startTile.__$movementLeft = movement;
-		startTile.__$cameFrom = null;
-		
-		
-		while(open.length != 0) {
-			var openTile = open.shift();
-			var openMovementLeft = openTile.__$movementLeft;
-			
-			var adjacentTiles = movementData.world.getAdjacentTiles(openTile);
-			
-			for(var i = 0; i < adjacentTiles.length; ++i) {
-				var tile = adjacentTiles[i];
-				
-				if (tile == openTile.__$cameFrom) {
-					continue;
-				}
-				
-				var terrainCost = movementData.placeable.CStatistics.terrainCost[tile.CTileTerrain.type];
-				if (terrainCost == undefined)
-					continue;
-				
-				var movementLeft = openMovementLeft - terrainCost;
-				var oldMovementLeft = tile.__$movementLeft || 0;
-				
-				// Check if has any placeable and its relation towards it.
-				var placedObject = tile.CTile.placedObjects[0];
-				var relation = PlayersData.Relation.Neutral;
-				if (placedObject) {
-					relation = movementData.playersData.getRelation(movementData.player, placedObject.CPlayerData.player);
-				}
-				
-				
-				if (oldMovementLeft <= movementLeft
-					&& relation != PlayersData.Relation.Enemy // Can't pass over enemies
-					&& movementLeft >= 0
-					) {
-					tile.__$movementLeft = movementLeft;
-					tile.__$cameFrom = openTile;
-					
-					if (open.indexOf(tile) == -1) {
-						open.push(tile);
-					}
-				}
-				
-			}
-			
-			
-			if (visited.indexOf(openTile) == -1) {
-				
-				// When searching for closed tiles, recent ones should be in front because they are more likely to be needed.
-				visited.unshift(openTile);
-				
-				// Can't stack units.
-				if (openTile != startTile && openTile.CTile.placedObjects.length == 0) {
-					availableTiles.push(openTile);
-				}
-			}
+	var movementCostQuery = function (userData, tile) {
+		var terrainCost = userData.placeable.CStatistics.terrainCost[tile.CTileTerrain.type];
+
+		// Check if has any placeable and its relation towards it.
+		var placedObject = tile.CTile.placedObjects[0];
+		var relation = PlayersData.Relation.Neutral;
+		if (placedObject) {
+			relation = userData.playersData.getRelation(userData.player, placedObject.CPlayerData.player);
 		}
-		
-		
-		// Cleanup algorithm data.
-		for(var i = 0; i < visited.length; ++i) {
-			delete visited[i].__$movementLeft;
-			delete visited[i].__$cameFrom;
-		}
+
+		return {
+			cost: terrainCost,
+			passOver: relation != PlayersData.Relation.Enemy, // Can't pass over enemies
+			discard: tile.CTile.placedObjects.length != 0,
+		};
 	}
-	
+
 };
