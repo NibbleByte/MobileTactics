20b27865b00766d23346e041ff21342c1fb468c5
Filip Slavov <2332996+NibbleByte@users.noreply.github.com>
2014-11-07 00:04:01 +0000

    * Added key-bindings for some Accept/Cancel buttons (mapped to Enter/ESC).

--- MobileClient/LocalFiles/Javascript/Main.js
+++ MobileClient/LocalFiles/Javascript/Main.js
@@ -45,6 +45,17 @@ $(function () {
 		currentState = ClientStateManager.changeState(ClientStateManager.types.TestGame);
 	}
 
+	$(document).keydown(function (e) {
+		if (e.which == 13) {	// Enter
+			e.preventDefault();
+			$('.btn_apply:visible').click();
+		}
+
+		if (e.which == 27) {	// ESC
+			e.preventDefault();
+			$('.btn_cancel:visible').click();
+		}
+	});
 	
 	// MoSync bindings
 	document.addEventListener("backbutton", close, true);
