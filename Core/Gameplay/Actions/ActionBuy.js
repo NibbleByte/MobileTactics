64e59ba6ef4911def76374b4de1fb3c2dd0b6269
Filip Slavov <2332996+NibbleByte@users.noreply.github.com>
2015-01-11 19:48:01 +0000

    * Buying units now has a special ActionBuy too, that is undoable. Yey!

---
+++ Core/Gameplay/Actions/ActionBuy.js
@@ -0,0 +1,28 @@
+"use strict";
+
+var Actions = Actions || {};
+Actions.Classes = Actions.Classes || {};
+
+// Action used to buy placeables, so it can have undo.
+Actions.Classes.ActionBuy = new function () {
+	
+	this.actionName = 'ActionBuy';
+	this.shouldRefreshVisibility = false;
+	
+	this.executeAction = function (eworld, world, action) {
+		
+		var placeable = action.placeable;
+
+		// TODO: Take players money. Take storeItem from action.undoData. Don't forget to undo it too.
+
+		placeable.CUnit.turnPoints = 0;
+		placeable.CUnit.finishedTurn = true;
+
+		eworld.addUnmanagedEntity(placeable);
+		world.place(placeable, action.appliedTile);
+	}
+
+	this.undoAction = function (eworld, world, action) {
+		eworld.removeManagedEntity(action.placeable);
+	}
+};
