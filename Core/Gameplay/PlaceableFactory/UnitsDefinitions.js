37a82dcac2d943b86d558e524e4384c4e56f2a8c
Filip Slavov <2332996+NibbleByte@users.noreply.github.com>
2015-08-09 14:50:12 +0000

    * Added example unit definitions and sprites for the JunkPeople race.

--- Core/Gameplay/PlaceableFactory/UnitsDefinitions.js
+++ Core/Gameplay/PlaceableFactory/UnitsDefinitions.js
@@ -276,10 +276,171 @@ UnitsDefinitions[Player.Races.Humans] = {
 };
 
 
-UnitsDefinitions[Player.Races.Roaches] = {
-};
 
 UnitsDefinitions[Player.Races.JunkPeople] = {
+
+	FlakTrooper: {
+		race: 0,
+		name: '@!@',
+		price: 100,
+
+		baseStatistics: {
+			Attack: 4,
+			AttackRange: 2,
+			HealRate: 1,
+			Defence: 3,
+			Movement: 3,
+			Visibility: 4,
+		},
+		
+		actions: [
+				  Actions.Classes.ActionCapture,
+				  Actions.Classes.ActionMove,
+				  Actions.Classes.ActionStay,
+				  Actions.Classes.ActionAttack,
+				  Actions.Classes.ActionHeal,
+				 ],
+		
+		terrainStats: new function () {
+			this[GameWorldTerrainType.Grass] =		{ Cost: 1, Attack: 0, Defence: 0};
+			this[GameWorldTerrainType.Dirt] =		{ Cost: 1, Attack: 0, Defence: 0};
+			this[GameWorldTerrainType.Forest] =		{ Cost: 2, Attack: 0, Defence: 1};
+			this[GameWorldTerrainType.Mountain] =	{ Cost: 3, Attack: 1, Defence: 2};
+
+			this[GameWorldTerrainType.Base] =		{ Cost: 1, Attack: 1, Defence: 2};
+			this[GameWorldTerrainType.Medical] =	{ Cost: 1, Attack: 1, Defence: 0};
+		},
+	},
+
+	Biker: {
+		race: 0,
+		name: '@!@',
+		price: 200,
+
+		baseStatistics: {
+			Attack: 5,
+			AttackRange: 1,
+			HealRate: 1,
+			Defence: 3,
+			Movement: 5,
+			Visibility: 6,
+			MovementAttack: 2,
+		},
+		
+		actions: [
+				  Actions.Classes.ActionMove,
+				  Actions.Classes.ActionStay,
+				  Actions.Classes.ActionAttack,
+				  Actions.Classes.ActionHeal,
+				 ],
+		
+		terrainStats: new function () {
+			this[GameWorldTerrainType.Grass] =		{ Cost: 1, Attack: 0, Defence: -1};
+			this[GameWorldTerrainType.Dirt] =		{ Cost: 1, Attack: 0, Defence: 0};
+			this[GameWorldTerrainType.Forest] =		{ Cost: 2, Attack: 1, Defence: 0};
+			this[GameWorldTerrainType.Mountain] =	{ Cost: 2, Attack: 1, Defence: 0};
+
+			this[GameWorldTerrainType.Base] =		{ Cost: 1, Attack: 0, Defence: -1};
+			this[GameWorldTerrainType.Medical] =	{ Cost: 1, Attack: 0, Defence: -1};
+		},
+	},
+
+	ScrapTank: {
+		race: 0,
+		name: '@!@',
+		price: 400,
+
+		baseStatistics: {
+			Attack: 10,
+			AttackRange: 1,
+			HealRate: 1,
+			Defence: 8,
+			Movement: 5,
+			Visibility: 6,
+		},
+		
+		actions: [
+				  Actions.Classes.ActionMove,
+				  Actions.Classes.ActionStay,
+				  Actions.Classes.ActionAttack,
+				  Actions.Classes.ActionHeal,
+				 ],
+		
+		terrainStats: new function () {
+			this[GameWorldTerrainType.Grass] =		{ Cost: 1, Attack: 0, Defence: 0};
+			this[GameWorldTerrainType.Dirt] =		{ Cost: 1, Attack: 0, Defence: 0};
+			this[GameWorldTerrainType.Forest] =		{ Cost: 2, Attack: -1, Defence: -2};
+
+			this[GameWorldTerrainType.Base] =		{ Cost: 1, Attack: 0, Defence: -1};
+			this[GameWorldTerrainType.Medical] =	{ Cost: 1, Attack: 0, Defence: -1};
+		},
+	},
+
+	Mech: {
+		race: 0,
+		name: '@!@',
+		price: 400,
+
+		baseStatistics: {
+			Attack: 10,
+			AttackRange: 1,
+			HealRate: 1,
+			Defence: 8,
+			Movement: 4,
+			Visibility: 6,
+		},
+		
+		actions: [
+				  Actions.Classes.ActionMove,
+				  Actions.Classes.ActionStay,
+				  Actions.Classes.ActionAttack,
+				  Actions.Classes.ActionHeal,
+				 ],
+		
+		terrainStats: new function () {
+			this[GameWorldTerrainType.Grass] =		{ Cost: 1, Attack: 0, Defence: 0};
+			this[GameWorldTerrainType.Dirt] =		{ Cost: 1, Attack: 0, Defence: 0};
+			this[GameWorldTerrainType.Forest] =		{ Cost: 2, Attack: 1, Defence: 1};
+			this[GameWorldTerrainType.Mountain] =	{ Cost: 2, Attack: 1, Defence: 1};
+
+			this[GameWorldTerrainType.Base] =		{ Cost: 1, Attack: 0, Defence: -1};
+			this[GameWorldTerrainType.Medical] =	{ Cost: 1, Attack: 0, Defence: -1};
+		},
+	},
+
+	Sting: {
+		race: 0,
+		name: '@!@',
+		price: 600,
+
+		baseStatistics: {
+			Attack: 10,
+			AttackRange: 5,
+			HealRate: 1,
+			Defence: 3,
+			Movement: 5,
+			Visibility: 6,
+		},
+		
+		actions: [
+				  Actions.Classes.ActionMove,
+				  Actions.Classes.ActionStay,
+				  Actions.Classes.ActionAttack,
+				  Actions.Classes.ActionHeal,
+				 ],
+		
+		terrainStats: new function () {
+			this[GameWorldTerrainType.Grass] =		{ Cost: 1, Attack: 0, Defence: -1};
+			this[GameWorldTerrainType.Dirt] =		{ Cost: 1, Attack: 0, Defence: -1};
+			this[GameWorldTerrainType.Forest] =		{ Cost: 2, Attack: -2, Defence: -1};
+
+			this[GameWorldTerrainType.Base] =		{ Cost: 1, Attack: 0, Defence: -1};
+			this[GameWorldTerrainType.Medical] =	{ Cost: 1, Attack: 0, Defence: 0};
+		},
+	},
+};
+
+UnitsDefinitions[Player.Races.Roaches] = {
 };
 
 (function () {
