8630476320138274442d1218c8db217680b0702a
NibbleByte <NibbleByte@655ab1b8-11a9-487b-b931-cf28db4b0e7f>
2015-09-29 16:39:30 +0000

    * Updated Forest01 sprite. Fixed terrain skins to work with animators (if name matches).

---
+++ trunk/MobileClient/www/Assets/Render/SpriteAnimations/World/Forest.js
@@ -0,0 +1,22 @@
+
+SpriteAnimations.World.Forest01 = {
+
+	resourcePath: 'Forest01.png',
+	
+	frameWidth: 64,
+	frameHeight: 80,
+
+	anchorY: 16,
+	
+	framesPerRow: 0,
+
+	speed: 12,
+	
+	sequences: [
+		{
+			name: 'Idle',
+			startIndex: 0,
+			frames: 1,
+		},
+	]
+};
