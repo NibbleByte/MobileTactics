9aa0fdd17f85e7fb525f55a47ab30ef67af7a23b
Filip Slavov <2332996+NibbleByte@users.noreply.github.com>
2015-12-16 18:18:20 +0000

    * Added Sting sprites.

--- MobileClient/www/Assets/Render/SpriteAnimations/FightUnits/FlakTrooperFight.js
+++ MobileClient/www/Assets/Render/SpriteAnimations/FightUnits/FlakTrooperFight.js
@@ -62,8 +62,8 @@ SpriteAnimations.FightUnits.FlakTrooper = {
 // TODO: These units don't have sprites yet. Remove this when they do!
 //SpriteAnimations.FightUnits.Biker = $.extend(true, {}, SpriteAnimations.FightUnits.FlakTrooper);
 //SpriteAnimations.FightUnits.ScrapTank = $.extend(true, {}, SpriteAnimations.FightUnits.FlakTrooper);
-SpriteAnimations.FightUnits.Mech = $.extend(true, {}, SpriteAnimations.FightUnits.FlakTrooper);
-SpriteAnimations.FightUnits.Sting = $.extend(true, {}, SpriteAnimations.FightUnits.FlakTrooper);
+//SpriteAnimations.FightUnits.Mech = $.extend(true, {}, SpriteAnimations.FightUnits.FlakTrooper);
+//SpriteAnimations.FightUnits.Sting = $.extend(true, {}, SpriteAnimations.FightUnits.FlakTrooper);
 
 
 SpriteAnimations.FightUnits.Biker = {
@@ -76,59 +76,6 @@ SpriteAnimations.FightUnits.Biker = {
 	speed: 14,
 	anchorX: Animator.AnchorX.Center,
 	anchorY: 140,
-	
-	sequences: [
-		{
-			name: 'Idle',
-			startIndex: 0,
-			frames: 1,
-		},
-		
-		
-		{
-			name: 'Idle0',
-			startIndex: 0,
-			frames: 1,
-		},
-		
-		{
-			name: 'Run',
-			startIndex: 0,
-			frames: 1,
-		},
-
-		{
-			name: 'Attack',
-			startIndex: 0,
-			frames: 1,
-			speed: 9,
-
-			events: [
-				
-				{
-					frame: 0,
-					event: FightRenderingEvents.Animations.FIRE,
-					params: {
-						weaponType: FightUnitWeaponType.Shotgun,
-						final: true,
-					}
-				},
-			],
-		},
-
-	]
-};
-
-SpriteAnimations.FightUnits.Mech = {
-
-	resourcePath: 'Mech.png',
-
-	frameWidth: 180,
-	frameHeight: 170,
-	framesPerRow: 5,
-	speed: 14,
-	anchorX: Animator.AnchorX.Center,
-	anchorY: 145,
 	
 	sequences: [
 		{
@@ -213,6 +160,112 @@ SpriteAnimations.FightUnits.ScrapTank = {
 				
 				{
 					frame: 2,
+					event: FightRenderingEvents.Animations.FIRE,
+					params: {
+						weaponType: FightUnitWeaponType.Shotgun,
+						final: true,
+					}
+				},
+			],
+		},
+
+	]
+};
+
+SpriteAnimations.FightUnits.Mech = {
+
+	resourcePath: 'Mech.png',
+
+	frameWidth: 180,
+	frameHeight: 170,
+	framesPerRow: 5,
+	speed: 14,
+	anchorX: Animator.AnchorX.Center,
+	anchorY: 145,
+	
+	sequences: [
+		{
+			name: 'Idle',
+			startIndex: 0,
+			frames: 1,
+		},
+		
+		
+		{
+			name: 'Idle0',
+			startIndex: 0,
+			frames: 1,
+		},
+		
+		{
+			name: 'Run',
+			startIndex: 0,
+			frames: 1,
+		},
+
+		{
+			name: 'Attack',
+			startIndex: 0,
+			frames: 1,
+			speed: 9,
+
+			events: [
+				
+				{
+					frame: 0,
+					event: FightRenderingEvents.Animations.FIRE,
+					params: {
+						weaponType: FightUnitWeaponType.Shotgun,
+						final: true,
+					}
+				},
+			],
+		},
+
+	]
+};
+
+SpriteAnimations.FightUnits.Sting = {
+
+	resourcePath: 'Sting.png',
+
+	frameWidth: 180,
+	frameHeight: 170,
+	framesPerRow: 5,
+	speed: 14,
+	anchorX: Animator.AnchorX.Center,
+	anchorY: 150,
+	
+	sequences: [
+		{
+			name: 'Idle',
+			startIndex: 0,
+			frames: 1,
+		},
+		
+		
+		{
+			name: 'Idle0',
+			startIndex: 0,
+			frames: 1,
+		},
+		
+		{
+			name: 'Run',
+			startIndex: 0,
+			frames: 1,
+		},
+
+		{
+			name: 'Attack',
+			startIndex: 0,
+			frames: 1,
+			speed: 9,
+
+			events: [
+				
+				{
+					frame: 0,
 					event: FightRenderingEvents.Animations.FIRE,
 					params: {
 						weaponType: FightUnitWeaponType.Shotgun,
