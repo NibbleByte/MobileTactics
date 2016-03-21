ebc86ee2c11e571f6a575d66b02b00a479ee45b7
Filip Slavov <2332996+NibbleByte@users.noreply.github.com>
2016-03-21 15:51:59 +0000

    * Created Game Toolbar, where in-game menu buttons will be. Quit and Next turn work for now. * Fixed: not starting the game in Test mode.

--- MobileClient/www/Javascript/Render/Systems/ControllerRenderingSystem.js
+++ MobileClient/www/Javascript/Render/Systems/ControllerRenderingSystem.js
@@ -4,7 +4,7 @@
 //===============================================
 "use strict";
 
-var ControllerRenderingSystem = function (m_renderer, m_$creditsLabel) {
+var ControllerRenderingSystem = function (m_renderer) {
 	var self = this;
 	
 	console.assert(m_renderer instanceof SceneRenderer, "SceneRenderer is required.");
@@ -17,22 +17,10 @@ var ControllerRenderingSystem = function (m_renderer, m_$creditsLabel) {
 	this.initialize = function () {
 		self._eworldSB.subscribe(ClientEvents.Controller.TILE_SELECTED, onTileSelected);
 
-		self._eworldSB.subscribe(GameplayEvents.Resources.CURRENT_CREDITS_CHANGED, onCreditsChanged);
-		self._eworldSB.subscribe(GameplayEvents.GameState.TURN_CHANGED, onTurnChanged);
-
-		self._eworldSB.subscribe(RenderEvents.FightAnimations.FIGHT_STARTED, hideCredits);
-		self._eworldSB.subscribe(RenderEvents.FightAnimations.FIGHT_FINISHED, showCredits);
-
 		m_selectedSprite = m_renderer.createSprite(WorldLayers.LayerTypes.Selection, ControllerRenderingSystem.TILE_SELECTED_SPRITE_PATH)
 		.size(GTile.TILE_WIDTH, GTile.TILE_HEIGHT);
 
 		onTileSelected(null);
-
-		m_$creditsLabel.show();
-	}
-
-	this.uninitialize = function () {
-		m_$creditsLabel.hide();
 	}
 
 	var onTileSelected = function (tile) {
@@ -49,27 +37,6 @@ var ControllerRenderingSystem = function (m_renderer, m_$creditsLabel) {
 			m_selectedSprite.hide();
 
 		self._eworld.trigger(RenderEvents.Layers.REFRESH_LAYER, WorldLayers.LayerTypes.Selection);
-	}
-
-	var onCreditsChanged = function (value, delta) {
-		m_$creditsLabel.text(value);
-	}
-
-
-	var onTurnChanged = function (gameState) {
-		if (gameState.currentPlayer && gameState.currentPlayer.type == Player.Types.Human) {
-			showCredits();
-		} else {
-			hideCredits();
-		}
-	}
-
-	var hideCredits = function () {
-		m_$creditsLabel.hide();
-	}
-
-	var showCredits = function () {
-		m_$creditsLabel.show();
 	}
 }
 
