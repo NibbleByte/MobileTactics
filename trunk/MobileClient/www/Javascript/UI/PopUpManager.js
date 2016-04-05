8b56ad687ae9c916347333afa9785430bfa28cfa
NibbleByte <NibbleByte@655ab1b8-11a9-487b-b931-cf28db4b0e7f>
2016-04-05 19:19:12 +0000

    * Added simple PopUpManager

---
+++ trunk/MobileClient/www/Javascript/UI/PopUpManager.js
@@ -0,0 +1,81 @@
+//===============================================
+// PopUpManager
+// 
+//===============================================
+"use strict";
+
+var PopUpManager = new function (m_element) {
+	var self = this;
+
+	var DEFAULT_PARAMS = {
+		title: 'Message',
+		message: '',
+		buttons: ['Ok'],
+		buttonsHandlers: null,
+		userData: null,
+	}
+
+	var m_$container = $('#PopUpDialog');
+	var m_$lbTitle = $('#PopUpDialogTitle').empty();
+	var m_$lbMessage = $('#PopUpDialogMessage').empty();
+	var m_$buttonsContainer = $('#PopUpDialogButtons').empty();
+
+	var m_requestsQueue = [];
+
+	var display = function (popUpData) {
+
+		m_$lbTitle.text(popUpData.title);
+		m_$lbMessage.text(popUpData.message);
+
+		m_$buttonsContainer.empty();
+
+		for(var i = 0; i < popUpData.buttons.length; ++i) {
+			var hotkeyClass = '';
+			if (i == 0) hotkeyClass = 'btn_apply';
+			if (i == popUpData.buttons.length - 1) hotkeyClass = 'btn_cancel';
+			if (i == 0 && popUpData.buttons.length == 1) hotkeyClass = 'btn_done';
+
+			$('<button>')
+			.text(popUpData.buttons[i])
+			.attr('ChoiceIndex', i)
+			.click(choiceHandler)
+			.addClass(hotkeyClass)
+			.appendTo(m_$buttonsContainer);
+		}
+	}
+
+	var choiceHandler = function (event) {
+
+		var choiceIndex = parseInt($(event.currentTarget).attr('ChoiceIndex'));
+
+		var popUpData = m_requestsQueue.shift();
+
+		if (m_requestsQueue.length == 0) {
+			m_$container.hide();
+		} else {
+			display(m_requestsQueue[0]);
+		}
+
+
+		if (!popUpData.buttonsHandlers)
+			return;
+
+		var userHandler = popUpData.buttonsHandlers[choiceIndex];
+
+		if (userHandler) userHandler(popUpData.userData);
+	}
+
+	this.show = function (popUpData) {
+
+		popUpData = $.extend({}, DEFAULT_PARAMS, popUpData);
+
+		m_requestsQueue.push(popUpData);
+		
+		if (m_requestsQueue.length == 1) {
+			display(m_requestsQueue[0]);
+		}
+
+		m_$container.show();
+	}
+
+};
