ce6b8c72193840a161739442e7410774bd3a16fa
NibbleByte <NibbleByte@655ab1b8-11a9-487b-b931-cf28db4b0e7f>
2016-04-15 12:36:14 +0000

    * Fixed: resources given to wrong player (wrong bases/minerals counting).

--- trunk/MobileClient/www/Javascript/ClientStates/TestGame.js
+++ trunk/MobileClient/www/Javascript/ClientStates/TestGame.js
@@ -270,8 +270,8 @@ ClientStateManager.registerState(ClientStateManager.types.TestGame, new function
 		m_eworld.addSystem(effects);
 		m_eworld.addSystem(new GameStateSystem());
 		m_eworld.addSystem(new VictoryStateMonitor());
-		m_eworld.addSystem(new ResourcesSystem());
 		m_eworld.addSystem(new TileStructuresSystem());	// Before TileVisibilitySystem, because structures also define visibility
+		m_eworld.addSystem(new ResourcesSystem());		// After TileStructuresSystem for correct current structures counting.
 		m_eworld.addSystem(new TileCapturingSystem());	// After TileStructuresSystem
 		m_eworld.addSystem(new TileVisibilitySystem(world));
 	
